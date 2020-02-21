0414e3043fb473f2f9137489917f1ab6079e4bc0
Petr Stepanov <petrstepvn@gmail.com>
2020-02-21 13:46:05 +0100

    Updates

---
+++ precache-manifest.b7eb4c040cd8491ea49b5027b87e63b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e9b45204d4740800170b7b89307d84",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "24d3bc8316eea16a422f",
+    "url": "/monsters/static/css/main.73130218.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/monsters/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d3bc8316eea16a422f",
+    "url": "/monsters/static/js/main.2538799d.chunk.js"
+  },
+  {
+    "revision": "16b38dd0445323191b24",
+    "url": "/monsters/static/js/runtime-main.b51bee9d.js"
+  }
+]);
